439f34ca1a6a19d38860203654d097f66794951a
Andriy Lobash <110089586+Ragnareock@users.noreply.github.com>
2023-04-28 22:54:56 +0300

    Task 01 hw 06 js done

---
+++ src/src/js/task-01.js
@@ -0,0 +1,11 @@
+const categoriesListEl = document.querySelector('#categories');
+
+const categoriesItemEl = categoriesListEl.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesItemEl.length}`);
+
+categoriesItemEl.forEach(function (itemEl) {
+    const titleOfCategories = itemEl.firstElementChild.textContent;
+    console.log(`Category: ${titleOfCategories}`);
+    const amountOfCategories = itemEl.lastElementChild.children.length;
+    console.log(`Elements: ${amountOfCategories}`);
+})
