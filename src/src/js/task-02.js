ad238908b3b1f3ef8c3c91d5b3519f913753e0db
Andriy Lobash <110089586+Ragnareock@users.noreply.github.com>
2023-04-30 00:03:27 +0300

    task 02 js done

--- src/src/js/task-02.js
+++ src/src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listItemEl = ingredients.map((elem) => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = elem;
+  itemEl.classList = 'item';
+  return itemEl;
+})
+
+const listEl = document.querySelector('#ingredients');
+listEl.append(...listItemEl);
