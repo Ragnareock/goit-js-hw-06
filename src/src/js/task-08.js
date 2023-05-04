57979ffcbebcc6520a8f2b90462c6da3fce6362c
Andriy Lobash <110089586+Ragnareock@users.noreply.github.com>
2023-05-05 01:37:14 +0300

    task 08 done

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault(); 
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('All fields must be completed');
+    }
+
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+
+} 
 
