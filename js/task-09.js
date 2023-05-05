f439a6c3f2a8ff2ca96f2f2e001c466806267690
Andriy Lobash <110089586+Ragnareock@users.noreply.github.com>
2023-05-05 20:32:50 +0300

    task 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnEl = document.querySelector('.change-color');
+btnEl.addEventListener('click', onChangeColor);
+const spanEl = document.querySelector('.color');
+
+function onChangeColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = document.body.style.backgroundColor;
+  console.log(spanEl.textContent);
+}
